f0a447ee611092bd6a6bb468486bd25f7e49d6f4
lokioh <loiclim.etu@gmail.com>
2023-03-25 19:38:45 +0100

    Adding the design header

---
+++ index.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Launch the start header of the tool
+./layout/startHeader.sh
+
+# Script for reconnaissance of a target system
+# Uses Nmap to scan for open ports and discover running services
+
+# Prompt user to enter IP address of target system
+#read -p "Enter the IP address of the target system: " ip_address
+
+# Use Nmap to scan for open ports on the target system
+#nmap -A $ip_address 
+
+# Wait for user input to continue
+#read -p "Press Enter to continue..."
---
+++ layout/startHeader.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script for layout SECURE block
+
+# Draw SECURE in large green letters using figlet
+echo -e "\033[32m"
+figlet SECURE
+
+# Print the meaning of SECURE
+echo "Security Evaluation and Compliance Unified Reporting Engine"
+
+# Reset the text color to default
+echo -e "\033[0m"
